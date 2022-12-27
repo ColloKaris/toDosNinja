33c7896535ac01a9101c3a212c2588a448f573c8
Collins Kariuki <collinsmunkary@gmail.com>
2022-12-27 18:16:07 +0300

    searching and filtering to do list

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos')
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input')
 
 const generateTemplate = (todo) => {
     const html = `
@@ -28,4 +29,31 @@ list.addEventListener('click', e => {
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
+})
+
+//function to match what is being typed with todos
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter((todo) => {
+            return !todo.textContent.toLowerCase().includes(term)
+        })
+        .forEach((todo) => {
+            todo.classList.add('filtered');
+        });
+
+        Array.from(list.children)
+        .filter((todo) => {
+            return todo.textContent.toLowerCase().includes(term)
+        })
+        .forEach((todo) => {
+            todo.classList.remove('filtered');
+        });
+}
+
+
+//KEYUP EVENT
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+    
 })
